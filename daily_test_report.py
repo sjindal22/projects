ef093b77a6aaec0b4bb644cc60c09297ade04a3c
Shivika Jindal <shivika.jindal@calix.com>
2017-06-04 13:29:51 -0500

    removed unused functions and print statements

--- daily_test_report.py
+++ daily_test_report.py
@@ -5,30 +5,15 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 import datetime
 import calendar
-
 plan_name = None
-
 
 class DailyTest:
     def __init__(self, plankey=None, max_result=None):
 
         self.plankey = plankey
         self.max_result = max_result
-        self.passed_tests = dict()
-        self.failed_tests = dict()
         self.pass_tests_date = dict()
         self.fail_tests_date = dict()
-        self.bamboo_failed_test = {}
-        self.bamboo_failed_versions_links = {}
-        self.new_passed_dict = {}
-        self.new_failed_dict = {}
-        self.version1 = []
-        self.version2 = []
-        self.final_label = []
-        self.passed_nums = []
-        self.failed_nums = []
-        self.list_of_urls = []
-        self.final_failed_nums = []
         self.plan_name = None
 
     def plan_build_state(self):
@@ -54,89 +39,6 @@ class DailyTest:
                 test_date_search = re.search("(\d{4}\-\d{2}\-\d{2})", str(dates))
                 self.fail_tests_date[test_date_search.group(1)] = self.fail_tests_date.get(test_date_search.group(1), 0) + 1
             i += 1
-
-        for key, value in self.bamboo_failed_test.items():
-            key1 = key.split('_')
-            key = key1[1]
-            self.bamboo_failed_versions_links[key] = value
-
-            # print ("Failed Test Run versions are" + str(self.bamboo_failed_versions_links))
-
-    def dict_without_prem_tags(self, prem_tagged_versions_dictionary, untagged_passed_dict, version):
-
-        for a, b in prem_tagged_versions_dictionary.items():
-            a = str(a)
-            a = a.split("_")
-            version.append(a[1])
-            untagged_passed_dict[a[1]] = b
-
-    def print_new_passed_failed_dict(self):
-        self.dict_without_prem_tags(self.passed_tests, self.new_passed_dict, self.version1)
-        self.dict_without_prem_tags(self.failed_tests, self.new_failed_dict, self.version2)
-
-    def sort_image_version_list(self):
-        val1 = []
-        parsed_version = []
-        sorted_list = []
-
-        for y, z in self.new_failed_dict.items():
-            if y not in self.version1:
-                self.version1.append(y)
-
-        for label_values in self.version1:
-            val1 = label_values.split("-")
-            parsed_version.append(int(val1[-1]))
-            sorted_list = sorted(parsed_version)
-
-        branch = val1[0] + '-' + val1[1] + '-' + val1[2]
-
-        for build_number in sorted_list:
-            version = branch + '-' + str(build_number)
-            self.final_label.append(version)
-
-            # print("\nFinal sorted list of labels is " + str(self.final_label))
-
-    def list_sorted_test_runs(self, new_untagged_dict, test_run_num_list):
-        j = 0
-        for items_in_list in self.final_label:
-            j = 0
-            for temp, values in new_untagged_dict.items():
-                if items_in_list == temp:
-                    test_run_num_list.append(values)
-                else:
-                    j += 1
-                    if j == len(new_untagged_dict.values()):
-                        test_run_num_list.append(0)
-                        j = 0
-
-    def failed_passed_list_nums(self):
-        self.list_sorted_test_runs(self.new_passed_dict, self.passed_nums)
-        self.list_sorted_test_runs(self.new_failed_dict, self.failed_nums)
-        # print ("\nPassed test run numbers " + str(self.passed_nums))
-        # print ("\nFailed test run numbers " + str(self.failed_nums))
-
-    def sort_failed_urls(self):
-        for items_in_list in self.final_label:
-            h = 0
-            m = 0
-            for items, valu in self.new_failed_dict.items():
-                if items_in_list == items:
-                    self.final_failed_nums.append(valu)
-                else:
-                    h += 1
-                    if h == len(self.new_failed_dict.values()):
-                        self.final_failed_nums.append(0)
-                        h = 0
-
-            for key1, val1 in self.bamboo_failed_versions_links.items():
-                if items_in_list == key1:
-                    self.list_of_urls.append(val1)
-                else:
-                    m += 1
-                    if m == len(self.bamboo_failed_versions_links.values()):
-                        self.list_of_urls.append(0)
-                        m = 0
-                        # print("Sorted failed urls " + str(self.list_of_urls))
 
     def plot_daily_result(self, pass_date, pass_count, fail_date, fail_count):
         trace1 = go.Bar(
@@ -172,12 +74,10 @@ class DailyTest:
                     color='black'
             )
 
-
         )
         )
         fig = plot(go.Figure(data=data, layout=layout), filename="plot_result.html")
         print(fig)
-
 
 def main():
     counter = 0
@@ -202,10 +102,6 @@ def main():
         strf_fail_time = []
 
         o.plan_build_state()
-        #o.print_new_passed_failed_dict()
-        #o.sort_image_version_list()
-        #o.failed_passed_list_nums()
-        #o.sort_failed_urls()
 
         for k, v in o.pass_tests_date.items():
             count = 0
@@ -222,7 +118,6 @@ def main():
                     if count == dates_count:
                         final_passed_dict[k] = v
                         break
-        print("Final passed date dictionary to be plotted is: " + str(final_passed_dict))
 
         for k, v in o.fail_tests_date.items():
             count = 0
@@ -239,7 +134,6 @@ def main():
                     if count == dates_count:
                         final_failed_dict[k] = v
                         break
-        print("Final failed date dictionary to be plotted is: " + str(final_failed_dict))
 
         for key, value in final_failed_dict.items():
             if key not in final_passed_dict.keys():
@@ -256,8 +150,9 @@ def main():
                     count_list.append(va)
                 else:
                     continue
-        print("sorted dates list for passed test runs" + str(strf_time))
-        print("Respective count for passed test runs on date " + str(count_list))
+                    
+        #print("sorted dates list for passed test runs" + str(strf_time))
+        #print("Respective count for passed test runs on date " + str(count_list))
 
         for key, value in final_passed_dict.items():
             if key not in final_failed_dict.keys():
@@ -275,15 +170,12 @@ def main():
                 else:
                     continue
 
-        print("sorted dates list for failed test runs " + str(strf_fail_time))
-        print("Respective count for failed test runs on date " + str(fail_count_list))
-        print("\n--")
-        print(len(strf_fail_time) is len(strf_time))
-        print(len(count_list) is len(fail_count_list))
-        print("\n--------")
+        #print("sorted dates list for failed test runs " + str(strf_fail_time))
+        #print("Respective count for failed test runs on date " + str(fail_count_list))
+        #print(len(strf_fail_time) is len(strf_time))
+        #print(len(count_list) is len(fail_count_list))
         if counter == queried_plans_list_length:
             o.plot_daily_result(strf_time, count_list, strf_fail_time, fail_count_list)
-
 
 if __name__ == "__main__":
     main()
