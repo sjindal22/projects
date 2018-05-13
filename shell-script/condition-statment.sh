bd16a8038826ae0d91ed31e756da31be6aa7ff2d
Shivika <shivika.jindal01@gmail.com>
2018-05-12 22:07:13 -0700

    updated script

--- shell-script/condition-statment.sh
+++ shell-script/condition-statment.sh
@@ -5,7 +5,12 @@ MY_SHELL="bash"
 if [ "$MY_SHELL" = "bash" ]
 then 
     echo "You seem to like the bash shell"
+
+elif [ $"MY_SHELL" = "csh" ]
+then
+    echo "You seem to like the csh shell"
+
 else
-    echo "You don't seem to like the bash shell"
+    echo "You don't seem to like either csh or bash shell"
 fi
 
